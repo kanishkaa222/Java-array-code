92c2b90cc22286805117dfb930e4a9f3b941eaec
Kanishka sahu <kanishkasahu344@gmail.com>
2026-06-05 13:25:56 +0530

    Add files via upload

---
+++ arrays2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+
+Scanner sc = new Scanner(System.in);
+int size = sc.nextInt();
+
+int numbers[] = new int[size];
+
+for(int i=0; i<size; i++){
+	numbers[i] = sc.nextInt();
+}
+ 
+  boolean isAscending = true;
+  
+for(int i=0; i<numbers.length-1; i++){
+  if (numbers[i] > numbers[i+1]) {
+  	isAscending = false;
+  }
+}
+
+if(isAscending){
+	System.out.println("array is sorted");
+}	else
+
+{
+	System.out.println("array is not sorted");
+}
+	}
+
+}
